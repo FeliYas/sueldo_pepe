f099f6f0f2f1efd9a4b6a75ebf7f05091db17b94
escuela@davinci.edu.ar <felipe.yaschischin@davinci.edu.ar>
2022-11-07 13:32:45 -0300

    chore: Empleado Interfacce

---
+++ src/EmpleadoI.java
@@ -0,0 +1,7 @@
+public interface EmpleadoI {
+    double calcularSueldoNeto();
+    double calcularBonoPorPresentismo(int diasAusente);
+    double calcularBonoPorResultado ();
+    double calcularSueldoTotal(int diasAusente);
+
+}
