8b55812348cd816ad1ac793be43a746a5032325c
escuela@davinci.edu.ar <felipe.yaschischin@davinci.edu.ar>
2022-11-07 13:33:13 -0300

    feat : Implenets cadetes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Sueldo Pepe");
+        Cadete pepe = new Cadete();
+        System.out.println(String.format("Sueldo total del empleado es %f",
+                pepe.calcularSueldoNeto()
+                ));
+
 }
 }
